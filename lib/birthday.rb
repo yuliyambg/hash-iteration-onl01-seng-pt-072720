93385fabccfefc0f406499422b2f656ae94fa015
secure-revert-8990 <yuliyambg@gmail.com>
2020-08-01 11:46:12 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -10,5 +10,10 @@ def happy_birthday(birthday_kids)
   end
 end
 
-def age_appropriate_birthday
+def age_appropriate_birthday(birthday_kids)
+
+  birthday_kids.each do |kids_name, age|
+    if age <12
+      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    end
 end
